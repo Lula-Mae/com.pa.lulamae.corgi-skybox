f787e1f587d59c5c9f4881a71f973c932a818be5
Lula-Mae <39922985+Lula-Mae@users.noreply.github.com>
2020-07-24 14:55:07 -0400

    Update settings.js

--- com.pa.lulamae.corgi-skybox/ui/mods/com.pa.lulamae.corgi-skybox/settings.js
+++ com.pa.lulamae.corgi-skybox/ui/mods/com.pa.lulamae.corgi-skybox/settings.js
@@ -2,7 +2,7 @@ try
 {
     if (model && model.skyBoxes)
     {
-        model.skyBoxes.push({text: 'Corgi Skybox by Lula Mae', value: '/pa/sky/terrain/textures/com.pa.lulamae.corgi-skybox/skybox.json'});
+        model.skyBoxes.push({text: 'Corgi by Lula Mae', value: '/pa/sky/terrain/textures/com.pa.lulamae.corgi-skybox/skybox.json'});
     }
 }
 catch (e)
